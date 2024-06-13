6e13f42123274ba03adacb8fbdecde76046a9d23
Piotr Wawrzyniak <piotrjuniorwawrzyniak@gmail.com>
2024-06-13 13:59:07 +0200

    Add view and template to display list of monitored sites

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import MonitoredSite
 
-# Create your views here.
+
+def index(request):
+    sites = MonitoredSite.objects.all()
+    context = {'sites': sites}
+    return render(request, 'blog/index.html', context)
