1f00b5ee10a51642a02342e81143d6ad248a635f
Piotr Wawrzyniak <piotrjuniorwawrzyniak@gmail.com>
2024-06-13 13:12:40 +0200

    Add MonitoredSite model and register it in admin

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class MonitoredSite(models.Model):
+    url = models.URLField(unique=True)
+    check_interval = models.IntegerField(help_text="Interval in minutes")
+    search_phrase = models.CharField(max_length=255, blank=True, null=True)
+    last_checked = models.DateTimeField(blank=True, null=True)
+    last_changed = models.DateTimeField(blank=True, null=True)
+    phrase_found = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.url
